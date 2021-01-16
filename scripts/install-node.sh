a9f95480d5cb56ace0af5d576a7ce8067c041dd5
dbennerIBM <dbenner@us.ibm.com>
2021-01-15 23:20:36 -0600

    Update install-node.sh

--- scripts/install-node.sh
+++ scripts/install-node.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+# Disable Swap
+swapoff -a
+
+# Checking Network Adapters
+## Letting iptables see bridged traffic
+sudo modprobe br_netfilter
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+
 # (Install Docker CE)
 ## Set up the repository:
 ### Install packages to allow apt to use a repository over HTTPS
