36d0101ee71a2b75c9d2861d0ea41c20e8a2d8a0
dbennerIBM <dbenner@us.ibm.com>
2021-01-18 16:01:02 -0600

    Update install-node.sh
    forgot the most important packages

--- scripts/install-node.sh
+++ scripts/install-node.sh
@@ -51,3 +51,5 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 cat <<EOF | sudo tee  >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
