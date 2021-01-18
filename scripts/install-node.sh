3f63d4f3de364b4b6d5a2e72f717f905345171e6
dbennerIBM <dbenner@us.ibm.com>
2021-01-18 15:20:11 -0600

    Update install-node.sh
    removed apt-get holds because they are maybe the real reason that the module 'rbd' is not able to load? testing now.

--- scripts/install-node.sh
+++ scripts/install-node.sh
@@ -51,6 +51,3 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 cat <<EOF | sudo tee  >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
